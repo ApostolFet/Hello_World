d6e35112b0391c14b03cea97f9451b0e25fca34a
apostol_fet <spb_it1@impermebel.ru>
2023-03-15 18:03:10 +0300

    print error in std:err

--- Rust/minigrep/src/main.rs
+++ Rust/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let envirement_args: Vec<String> = env::args().collect();
 
     let config = Config::build(&envirement_args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Aplication error: {e}");
+        eprintln!("Aplication error: {e}");
         process::exit(1);
     }
 }
