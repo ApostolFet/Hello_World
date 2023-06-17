83baa49cdf4c0918302bfcedc1484991488b3662
Apostol Fet <maksim170901@gmail.com>
2023-06-18 00:10:04 +0300

    implemented quicksort on Rust

--- Rust/algoritm/src/main.rs
+++ Rust/algoritm/src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let result_max = max(numbers);
     assert_eq!(result_max, 9);
+
+    let numbers = vec![10, 8, 2, 6, 3, 6, 1, 4, 7, 5, 9];
+    let result_max = quicksort(numbers);
+    assert_eq!(result_max, vec![1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10]);
 }
 
 fn sum(list: Vec<isize>) -> isize {
@@ -53,12 +57,35 @@ fn max(list: Vec<isize>) -> isize {
 
         let max_value_tail = max(tail);
 
-        let mut max_value: isize = 0;
+        let max_value: isize;
         if max_value_tail < list[0] {
             max_value = list[0];
         } else {
             max_value = max_value_tail;
         }
         return max_value;
+    }
+}
+
+fn quicksort(mut list: Vec<isize>) -> Vec<isize> {
+    if list.len() < 2 {
+        return list;
+    } else {
+        let mut head = Vec::new();
+        let mut tail = Vec::new();
+
+        let main_element = list.pop().unwrap();
+        for i in list {
+            if i > main_element {
+                head.push(i);
+            } else {
+                tail.push(i);
+            }
+        }
+        let mut result = Vec::new();
+        result.extend(quicksort(tail));
+        result.extend(vec![main_element]);
+        result.extend(quicksort(head));
+        return result;
     }
 }
