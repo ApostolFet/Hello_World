7b3ac94b52565dee58f1dbcb5b26fa8f235ac7f3
Apostol Fet <maksim170901@gmail.com>
2024-06-04 00:33:06 +0300

    feature: add generics, maxlen, pop return value, overload for getitem

--- Python/LinkedList/linked_list.py
+++ Python/LinkedList/linked_list.py
@@ -2,18 +2,21 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import Any, Self, SupportsIndex
+from typing import Generic, Self, SupportsIndex, TypeVar, overload
 from operator import index
 
 
+T = TypeVar("T")
+
+
 @dataclass
-class Node:
-    value: Any
-    head: Node | None = None
-    tail: Node | None = None
+class Node(Generic[T]):
+    value: T
+    head: Node[T] | None = None
+    tail: Node[T] | None = None
 
 
-class LinkedList:
+class LinkedList(Generic[T]):
     """
     >>> linked_list = LinkedList((1, 2, 3, 4, 5))
     >>> list(linked_list)
@@ -28,10 +31,12 @@ class LinkedList:
     [0, 1, 2, 3, 4, 5, 6]
 
     >>> linked_list.pop()
+    6
     >>> list(linked_list)
     [0, 1, 2, 3, 4, 5]
 
     >>> linked_list.pop_left()
+    0
     >>> list(linked_list)
     [1, 2, 3, 4, 5]
 
@@ -83,17 +88,42 @@ class LinkedList:
     Traceback (most recent call last):
         ...
     IndexError: pop from empty linked list
+
+    >>> max_len_linked_list = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], maxlen=3)
+    >>> list(max_len_linked_list)
+    [8, 9, 10]
+    >>> max_len_linked_list.append(11)
+    >>> list(max_len_linked_list)
+    [9, 10, 11]
+    >>> max_len_linked_list.append_left(8)
+    >>> list(max_len_linked_list)
+    [8, 9, 10]
+
+    >>> LinkedList([1, 2, 3], maxlen=-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: maxlen must be non-negative
+
+    get last 10 values from generator:
+    >>> list(LinkedList((i for i in range(1001)), maxlen=10))
+    [991, 992, 993, 994, 995, 996, 997, 998, 999, 1000]
     """
 
-    def __init__(self, iterable: Iterable = tuple()) -> None:
+    def __init__(
+        self, iterable: Iterable[T] = tuple(), maxlen: int | None = None
+    ) -> None:
         self._head = None
         self._tail = None
         self._len = 0
 
+        if maxlen is not None and maxlen < 0:
+            raise ValueError("maxlen must be non-negative")
+        self._maxlen = maxlen
+
         for value in iterable:
             self.append(value)
 
-    def append(self, value: Any) -> None:
+    def append(self, value: T) -> None:
         new_node = Node(value=value, head=self._tail, tail=None)
 
         if self._tail is not None:
@@ -104,8 +134,10 @@ class LinkedList:
             self._head = self._tail
 
         self._len += 1
+        if self._maxlen is not None and self._len > self._maxlen:
+            self.pop_left()
 
-    def append_left(self, value: Any) -> None:
+    def append_left(self, value: T) -> None:
         new_node = Node(value=value, head=None, tail=self._head)
 
         if self._head is not None:
@@ -117,9 +149,14 @@ class LinkedList:
 
         self._len += 1
 
-    def pop(self) -> None:
+        if self._maxlen is not None and self._len > self._maxlen:
+            self.pop()
+
+    def pop(self) -> T:
         if self._tail is None:
             raise IndexError("pop from empty linked list")
+
+        value = self._tail.value
 
         self._tail = self._tail.head
         if self._tail is not None:
@@ -127,19 +164,28 @@ class LinkedList:
 
         self._len -= 1
 
-    def pop_left(self) -> None:
+        return value
+
+    def pop_left(self) -> T:
         if self._head is None:
             raise IndexError("pop from empty linked list")
+
+        value = self._head.value
         self._head = self._head.tail
         if self._head is not None:
             self._head.head = None
 
         self._len -= 1
+        return value
 
     def __len__(self) -> int:
         return self._len
 
-    def __getitem__(self, i: SupportsIndex | slice) -> Self | Any:
+    @overload
+    def __getitem__(self, i: slice) -> Self: ...
+    @overload
+    def __getitem__(self, i: SupportsIndex) -> T: ...
+    def __getitem__(self, i: slice | SupportsIndex) -> Self | T:
         if isinstance(i, slice):
             return self._get_slice(i)
         return self._get_by_index(i)
@@ -155,7 +201,7 @@ class LinkedList:
                 break
         return cls(values)
 
-    def _get_by_index(self, i: SupportsIndex) -> Any:
+    def _get_by_index(self, i: SupportsIndex) -> T:
         if self._head is None:
             raise IndexError
 
@@ -167,6 +213,8 @@ class LinkedList:
         current_value = current_node.value
 
         for _ in range(i):
+            if current_node.tail is None:
+                raise IndexError("index out of range")
             current_node = current_node.tail
             try:
                 current_value = current_node.value
