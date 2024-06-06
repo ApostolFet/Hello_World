9750892456ca5932898edc0babc86b5dbfed3662
apostol_fet <spb_it1@impermebel.ru>
2024-06-06 11:17:45 +0300

    refactor: realizantion introduce by method __str__

--- Python/School/human.py
+++ Python/School/human.py
@@ -3,8 +3,15 @@ class Human:
         self.name = name
         self.sex = sex
 
+    def __str__(self) -> str:
+        return f"Привет, я {self.sex}, меня зовут {self.name}."
+
+    def __repr__(self) -> str:
+        cls = type(self)
+        return f"{cls.__name__}({self.name}, {self.sex})"
+
     def introduce(self):
-        print(f"Привет, я {self.sex}, меня зовут {self.name}.")
+        print(self)
 
 
 class SoftwareDeveloper(Human):
@@ -12,8 +19,8 @@ class SoftwareDeveloper(Human):
         super().__init__(name, sex)
         self.language = language
 
-    def introduce(self):
-        print(
-            f"Привет, я {self.sex}, меня зовут {self.name}."
-            f" Я пишу на {self.language}"
-        )
+    def __str__(self) -> str:
+        return super().__str__() + f" Я пишу на {self.language}"
+
+    def __repr__(self) -> str:
+        return super().__repr__()[:-1] + f", {self.language})"
